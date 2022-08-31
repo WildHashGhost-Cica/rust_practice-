bb9860205c16d715ca9898060efd825575af089b
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-08-31 17:45:23 +0100

    if ... else

---
+++ src/bin/a3.rs
@@ -0,0 +1,15 @@
+//Flow control using if ... else
+
+
+
+
+fn main(){
+    //Displays a message based on the value of boolean variable
+    let my_bool  = true;
+    //When the variable is set to true, display "hello"
+    if my_bool == false {
+        println!("Hello")
+    } else {
+        println!("Goodbye")
+    }
+}
