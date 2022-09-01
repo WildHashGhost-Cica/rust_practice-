3317a51400f521cf69e4a820460027e0adc1b930
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-01 15:57:29 +0100

    match

---
+++ src/bin/match.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let my_name = "Leon";
+    match my_name {
+        "Cica" => println!("That is my name"),
+        "Jayson" => println!("not my name"),
+        "Alex" => println!("Hello Alex"),
+        _ => println!("nice to meet you!")
+    }
+}
