0c62a2d7a7407c7e39f5b28195a71622ad542aa2
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-01 16:28:28 +0100

    match

--- src/bin/match.rs
+++ src/bin/match.rs
@@ -11,4 +11,11 @@ fn main() {
         true => println!("it's true"),
         false => println!("it's false"),
     }
+    let my_number = 2;
+    match my_number {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other"),
+    }
 }
