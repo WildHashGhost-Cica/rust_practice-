997dbc9ae8b9e906eedf33ffe2a1c265e6e7a9a5
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-05 19:08:48 +0100

    enum

--- src/bin/enum.rs
+++ src/bin/enum.rs
@@ -4,6 +4,20 @@ enum Direction{
     Left,
     Right
 }
+enum Color{
+    Red,
+    Black,
+    Green
+}
+
+fn print_color(my_color: Color){
+    match my_color{
+        Color::Red => println!("red"),
+        Color::Black => println!("black"),
+        Color::Green => println!("green"),
+    }
+}
+
 fn main() {
     let go = Direction::Left;
     match go {
@@ -12,4 +26,5 @@ fn main() {
         Direction::Left => println!("left"),
         Direction::Right => println!("right"),
     }
+    print_color(Color::Black);
 }
