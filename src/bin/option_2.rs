0d714493f33d05765e605ec206c5fc5d63a6fcce
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-15 18:25:01 +0100

    option_2

--- src/bin/option_2.rs
+++ src/bin/option_2.rs
@@ -6,12 +6,20 @@ struct Survey {
 
 fn main(){
     let response = Survey {
-        q1: Some(12),
+        q1: None,
         q2: Some(true),
         q3: Some("A".to_owned()),
     };
     match response.q1 {
         Some(ans) => println!("q1: {:?}", ans),
         None => println!("q1: no response"),
+    }
+    match response.q2 {
+        Some(ans) => println!("q2: {:?}", ans),
+        None => println!("q2: no response"),
+    }
+    match response.q3 {
+        Some(ans) => println!("q3: {:?}", ans),
+        None => println!("q3: no response"),
     }
 }
