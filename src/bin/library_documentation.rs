7dac66dfb30325236efc77a7a7939de4a2f87789
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-16 19:07:20 +0100

    library documentation

---
+++ src/bin/library_documentation.rs
@@ -0,0 +1,7 @@
+// write rustup doc in the terminal
+
+fn main(){
+    let my_str = "this is my STRING";
+    println!("uppercase: {:?}", my_str.to_uppercase());
+    println!("lowercase:{:?}", my_str.to_lowercase());
+}
