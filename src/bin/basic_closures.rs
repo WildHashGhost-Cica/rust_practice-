848f9b0352206fde10fa9745eac782ee41408640
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-22 14:25:39 +0100

    basic closures

---
+++ src/bin/basic_closures.rs
@@ -0,0 +1,12 @@
+/*fn add(a: i32, b: i32) -> i32 {
+    a + b
+}*/
+
+fn main(){
+   /* let add = |a: i32, b:i32| -> i32 {
+        a + b
+    };*/
+    let add =  |a, b| a +b;
+    let sum = add(10,1);
+    println!("{:?}", sum);
+}
